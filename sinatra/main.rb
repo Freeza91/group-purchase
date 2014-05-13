ddf980dec6ea243819a983ebd6957dbaffea704a
hunter <347212291@qq.com>
2014-05-13 14:33:45 +0800

    update user

--- sinatra/main.rb
+++ sinatra/main.rb
@@ -6,15 +6,96 @@ require 'yaml'
 require './model/models'
 require './config/db_config'
 require './helpers/helper'
+require 'json'
+require 'securerandom'
 
 setting = YAML::load(File.open('./config/en.yml'))
 
 get '/' do 
-  user = User.create(
-                     :username => "username", :password => "password",
-                     :token => 'token'
-                     )
-	"hello world"
+  user = User.all
+  user.each do |u|
+    u.destroy!
+  end
+end
+
+
+
+post '/users/create' do
+
+  token = SecureRandom.uuid
+  username = params[:username]
+  password = params[:password]
+  tel = params[:tel]
+  p username, password, tel
+  user = User.create(:username => username, :password => password, :tel => tel, :token => token)
+
+  reply = {}
+  reply['message'] = 'success'
+  reply['token'] = token
+  reply.to_json
+  
+end
+
+post '/users/login' do
+  password = params[:password]
+  tel = params[:tel]
+  user = User.find_by(:tel => tel)
+  reply = {}
+  if user and user.password == password
+    reply['message'] = 'success'
+    reply['token'] = user.token
+  else 
+    reply['message'] = 'failed'
+  end
+  reply.to_json
+end
+
+put '/users/update/username' do
+  username = params['username']
+  token = params['token']
+  reply = {}
+  p 123
+  user = User.find_by(:token => token)
+  if user 
+    token = SecureRandom.uuid
+    user.update(:username => username, :token => token)
+    reply['message'] = 'success'
+    reply['token'] =  token
+  else
+    reply['message'] = 'failed'
+  end
+  reply.to_json
+
+end
+
+put '/users/update/password' do
+  password = params['password']
+  token = params['token']
+  reply = {}
+
+  user = User.find_by(:token => token)
+  if user 
+    token = SecureRandom.uuid
+    user.update(:token => token, :password => password)
+    reply['message'] = 'success'
+    reply['token'] =  token
+  else
+    reply['message'] = 'failed'
+  end
+  reply.to_json
+end
+
+post '/users' do
+  token = params[:token]
+  user = User.find_by(:token => token)
+  reply = {}
+  if user 
+    reply['message'] = 'success'
+    reply['username'] = user.username
+  else
+    reply['message'] = 'failed'
+  end
+  reply.to_json
 end
 
 
--- sinatra/model/models.rb
+++ sinatra/model/models.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+  #attr_accessor :tel, :password, :username, :token
+
+
+
+
+   def to_json
+    hash = {}
+    self.instance_variables.each do |var|
+      hash[var.to_s.split('@')[1]] = self.instance_variable_get var
+    end
+    JSON.parse hash.to_json
+  end
 end
-
-
